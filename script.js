4e6c78b6e35d3367eeea8f9a004f2eb3b7a55430
tariklaroda <122169007+tariklaroda@users.noreply.github.com>
2023-05-25 14:20:51 -0300

    Made ui implementation
    - buttons are used instead of text,
    - winners are displayed on screen
    - score is displayed
    - score is reset after each game

--- script.js
+++ script.js
@@ -32,20 +32,26 @@ function playRound(playerSelection, computerSelection) {
   //initialize computer selection
   const cSelection = computerSelection;
 
+  //change the div
+  const results = document.getElementById("results");
+
   //determine winner
 
   if (pSelection === cSelection) {
     console.log("It is a draw");
+    results.innerHTML = "It is a draw!";
   } else if (
     (pSelection === "rock" && cSelection === "scissors") ||
     (pSelection === "paper" && cSelection === "rock") ||
     (pSelection === "scissors" && cSelection === "paper")
   ) {
     console.log("You WIN! ", pSelection, " beats ", cSelection);
+    results.innerHTML = `You WIN! ${pSelection} beats ${cSelection}`;
     //if player wins return 0
     return 0;
   } else {
     console.log("You Lose! ", cSelection, " beats ", pSelection);
+    results.innerHTML = `You Lose! ${cSelection} beats ${pSelection}`;
     //if player loses return 1
     return 1;
   }
@@ -61,23 +67,47 @@ function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
-  //play round
-  for (let i = 0; i < NUM_ROUNDS; i++) {
-    //get selection
-    const playerSelection = prompt();
-    const computerChoice = getComputerChoice();
+  const paperBtn = document.querySelector("#paperBtn");
+  const scissorsBtn = document.querySelector("#scissorsBtn");
+  const rockBtn = document.querySelector("#rockBtn");
+  const score = document.getElementById("score");
 
-    // play round
-    let roundRes = playRound(playerSelection, computerChoice);
+  function getRoundWinnerAndScore(pSelection) {
+    let roundRes = playRound(pSelection, getComputerChoice());
     if (roundRes == 0) {
       playerScore++;
     } else if (roundRes == 1) {
       computerScore++;
     }
+    console.log("Player: ", playerScore, "\tComputer: ", computerScore);
+    score.innerHTML = `Player: ${playerScore}\t Computer: ${computerScore}`;
+
+    if (playerScore > 4 || computerScore > 4) {
+      console.log("The game is over!");
+
+      if (playerScore > 4) {
+        console.log("Congradulations! You WON the game!");
+        results.innerHTML = `Congradulations! You WON the game!`;
+      } else {
+        console.log("You LOST the game! Better luck next time!");
+        results.innerHTML = `You LOST the game! Better luck next time!`;
+      }
+      //set scores to 0
+      playerScore = 0;
+      computerScore = 0;
+      score.innerHTML = `Player: ${playerScore}\t Computer: ${computerScore}`;
+    }
   }
 
-  //display results
-  console.log("Player: ", playerScore, "\tComputer: ", computerScore);
+  rockBtn.addEventListener("click", function () {
+    getRoundWinnerAndScore("rock");
+  });
+  scissorsBtn.addEventListener("click", function () {
+    getRoundWinnerAndScore("scissors");
+  });
+  paperBtn.addEventListener("click", function () {
+    getRoundWinnerAndScore("paper");
+  });
 }
 
 //call main function
